bc8b02f26a259fde7dcc020fa88023a2211296b2
sqrtofsaturn <sqrtofsaturn@gmail.com>
2017-03-10 13:57:14 -0700

    warn on deploy

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,25 @@ fatal() {
 }
 
 main() {
-  local tag="$1"
-  if [ -z "$tag" ]; then
-    fatal "Missing tag as first argument"
+  echo "#######################################################################"
+  echo "# WARNING: You may now gump and codefresh will deploy automagically!! #"
+  echo "#######################################################################"
+  echo 
+  read -p "Are you sure you want to manually deploy? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    local tag="$1"
+    if [ -z "$tag" ]; then
+      fatal "Missing tag as first argument"
+    fi
+    echo "building $LOCAL_IMAGE_NAME:$tag"
+    ./build.sh "$tag" || fatal 'build failed'
+    echo "tagging $LOCAL_IMAGE_NAME:$tag -> $REMOTE_IMAGE_NAME:$tag"
+    docker tag "$LOCAL_IMAGE_NAME:$tag" "$REMOTE_IMAGE_NAME:$tag" || fatal 'failed to tag'
+    echo "pushing $REMOTE_IMAGE_NAME:$tag"
+    docker push "$REMOTE_IMAGE_NAME:$tag" || fatal 'failed to push'
   fi
-  echo "building $LOCAL_IMAGE_NAME:$tag"
-  ./build.sh "$tag" || fatal 'build failed'
-  echo "tagging $LOCAL_IMAGE_NAME:$tag -> $REMOTE_IMAGE_NAME:$tag"
-  docker tag "$LOCAL_IMAGE_NAME:$tag" "$REMOTE_IMAGE_NAME:$tag" || fatal 'failed to tag'
-  echo "pushing $REMOTE_IMAGE_NAME:$tag"
-  docker push "$REMOTE_IMAGE_NAME:$tag" || fatal 'failed to push'
 }
 
 main "$@"
